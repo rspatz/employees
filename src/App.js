5c00155e167cd18f63be0c868a5315ad70f653ff
Robert <rspatz@live.com>
2024-11-17 08:34:31 -0600

    Begin building application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
+import Cardlist from './components/card-list/card-list.component';
+import Searchbox from './components/search-box/search-box.component';
 import './App.css';
 
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      employees = [],
+      searchField: '',
+    };
+    console.log('constructor');
+  }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
